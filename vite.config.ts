86579d4726e110db17e13f24c2850b758737537f
Farid Tavakoli <89019161+FaridTvK@users.noreply.github.com>
2024-11-18 14:25:34 +0100

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    base: ""
+    base: "",
+    root: "src",
+    build: {
+        rollupOptions: {
+            input: {
+                main: "src/mein.html"
+            }
+        }
+    }
 })
 
